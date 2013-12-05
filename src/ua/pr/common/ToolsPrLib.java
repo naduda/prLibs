fec83df8551e7b14702a463a977d419c0a326d18
naduda <naduda.pr@gmail.com>
2013-12-05 14:58:39 +0200

    add getFindingText(String text, String begin, String end) in ToolsPrLib
    Повертає список рядків, які знаходяться між вказаними символами begin і
    end
    List<String> res = getFindingText("qwe [first] dasg [second] asd", "[",
    "]")

--- src/ua/pr/common/ToolsPrLib.java
+++ src/ua/pr/common/ToolsPrLib.java
@@ -1,5 +1,13 @@
 package ua.pr.common;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * @author pavel.naduda
+ *
+ */
 public class ToolsPrLib {
 	
 	/*
@@ -34,5 +42,24 @@ public class ToolsPrLib {
 		}
 		
 		return res.replace("\\", "/");
+	}	
+	
+	/**
+	 * Повертає список рядків, які знаходяться між вказаними символами begin і end
+	 * List<String> res = getFindingText("qwe [first] dasg [second] asd", "[", "]")
+	 */
+	public static List<String> getFindingText(String text, String begin, String end) {
+		List<String> ret = new ArrayList<>();
+		if (text.indexOf(begin) != -1) {
+			StringTokenizer st = new StringTokenizer(text.substring(text.indexOf(begin)), begin);
+			while (st.hasMoreElements()) {
+				String temp = (String) st.nextElement();
+				if (temp.indexOf(end) != -1) {
+					ret.add(temp.substring(0, temp.indexOf(end)));
+				}
+			}
+		}
+		
+		return ret;
 	}
 }
